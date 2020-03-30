79c227d004860d8c99f9e1b69012a71a5c02919b
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-03-30 14:16:01 -0700

    coding

--- forfun.py
+++ forfun.py
@@ -1,9 +1,17 @@
-from seg.newline.segmenter import NewLineSegmenter
-import spacy
+# from seg.newline.segmenter import NewLineSegmenter
+# import spacy
+#
+# nlseg = NewLineSegmenter()
+# nlp = spacy.load('en')
+# nlp.add_pipe(nlseg.set_sent_starts, name='sentence_segmenter', before='parser')
+# my_doc_text = "Donald John Trump is the 45th and current president of the United States. Before entering politics, he was a businessman and television personality. Trump was born and raised in Queens, a borough of New York City, and received a bachelor's degree in economics from the Wharton School."
+# doc = nlp(my_doc_text)
+# print(doc)
 
-nlseg = NewLineSegmenter()
-nlp = spacy.load('en')
-nlp.add_pipe(nlseg.set_sent_starts, name='sentence_segmenter', before='parser')
-my_doc_text = "Donald John Trump is the 45th and current president of the United States. Before entering politics, he was a businessman and television personality. Trump was born and raised in Queens, a borough of New York City, and received a bachelor's degree in economics from the Wharton School."
-doc = nlp(my_doc_text)
-print(doc)
+
+import spacy
+nlp = spacy.load('en_core_web_sm')
+text = "Donald John Trump is the 45th and current president of the United States. Before entering politics, he was a businessman and television personality. Trump was born and raised in Queens, a borough of New York City, and received a bachelor's degree in economics from the Wharton School."
+text_sentences = nlp(text)
+for sentence in text_sentences.sents:
+    print(sentence.text)
