1fb873f10eaa6881f8ac9b56630e3ff779d96bc7
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-03-31 10:19:15 -0700

    coding

--- forfun.py
+++ forfun.py
@@ -4,7 +4,7 @@ import torch
 tokenizer = AutoTokenizer.from_pretrained("distilbert-base-cased")
 model = AutoModelWithLMHead.from_pretrained("distilbert-base-cased")
 
-sequence = f"Distilled models are smaller than the models they mimic. Using them instead of the large versions would help {tokenizer.mask_token} our carbon footprint."
+sequence = f"Distilled models are smaller than the models they mimic. Using them instead of the large versions would help {tokenizer.mask_token} our carbon footprint. Distilled models are smaller than the models they mimic. Using them instead of the large versions would help Distilled models are smaller than the models they mimic. Using them instead of the large versions would help Distilled models are smaller than the models they mimic. Using them instead of the large versions would help Distilled models are smaller than the models they mimic. Using them instead of the large versions would help Distilled models are smaller than the models they mimic. Using them instead of the large versions would help Distilled models are smaller than the models they mimic. Using them instead of the large versions would help"
 
 input = tokenizer.encode(sequence, return_tensors="pt")
 mask_token_index = torch.where(input == tokenizer.mask_token_id)[1]
