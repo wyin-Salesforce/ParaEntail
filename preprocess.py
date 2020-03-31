5dbecb8f999a1bf5d9b2bfd221c1d27e4ea7a6b9
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-03-30 22:08:27 -0700

    coding

--- preprocess.py
+++ preprocess.py
@@ -29,7 +29,7 @@ def load_CNN_DailyMail():
     gpt2_tokenizer = AutoTokenizer.from_pretrained("gpt2")
     gpt2_model = AutoModelWithLMHead.from_pretrained("gpt2")
 
-    file_prefix = ['val']#['train', 'val', 'test']
+    file_prefix = ['test']#['train', 'val', 'test']
     for fil_prefix in file_prefix:
         readfil = '/export/home/Dataset/CNN-DailyMail-Summarization/split/'+fil_prefix+'_tokenized.txt'
         writefil = '/export/home/Dataset/para_entail_datasets/CNN_DailyMail/'+fil_prefix+'_in_entail.txt'
