517aed7c63df8a2fdf187dafac380bb4ea11f7d7
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-04-06 23:06:00 -0700

    coding

--- load_data.py
+++ load_data.py
@@ -42,7 +42,7 @@ def get_DUC_examples(prefix):
 
     print('>>pos:neg: ', pos_size, neg_size)
     print('size:', len(examples))
-    return examples
+    return examples, pos_size
 
 
 def get_CNN_DailyMail_examples(prefix):
@@ -84,12 +84,15 @@ def get_CNN_DailyMail_examples(prefix):
 
     print('>>pos:neg: ', pos_size, neg_size)
     print('size:', len(examples))
-    return examples
+    return examples, pos_size
 
 def get_MCTest_examples(prefix):
     path = '/export/home/Dataset/para_entail_datasets/MCTest/'
-    readfile = codecs.open(path+prefix+'_in_entail.txt', 'r', 'utf-8')
+    filename = path+prefix+'_in_entail.txt'
+    print('loading MCTest...', filename)
+    readfile = codecs.open(filename, 'r', 'utf-8')
     guid_id = 0
+    pos_size = 0
     examples = []
     for line in readfile:
         guid_id+=1
@@ -98,9 +101,11 @@ def get_MCTest_examples(prefix):
             premise = parts[1]
             hypothesis = parts[2]
             label = parts[0]
+            if label == 'entailment':
+                pos_size+=1
             examples.append(InputExample(guid=prefix+str(guid_id), text_a=premise, text_b=hypothesis, label=label))
     print('size:', len(examples))
-    return examples
+    return examples, pos_size
 
 def get_FEVER_examples(prefix):
     '''
@@ -109,50 +114,60 @@ def get_FEVER_examples(prefix):
     examples = []
     path = '/export/home/Dataset/para_entail_datasets/nli_FEVER/nli_fever/'
     filename = path+prefix+'_fitems.jsonl'
+    print('loading FEVER...', filename)
     guid_id = 0
+    pos_size = 0
     with open(filename, 'r') as f:
         for line in json_lines.reader(f):
             guid_id+=1
             premise = line.get('context')
             hypothesis = line.get('query')
             label = 'entailment' if line.get('label') == 'SUPPORTS' else 'non_entailment'
+            if label == 'entailment':
+                pos_size+=1
             examples.append(InputExample(guid=str(guid_id), text_a=premise, text_b=hypothesis, label=label))
     print('size:', len(examples))
-    return examples
+    return examples, pos_size
 
 def get_ANLI_examples(prefix):
     folders = ['R1', 'R2', 'R3']
     examples = []
     guid_id = 0
+    pos_size = 0
     path = '/export/home/Dataset/para_entail_datasets/ANLI/anli_v0.1/'
     for folder in folders:
-        with open(path+folder+'/'+prefix+'.jsonl', 'r') as f:
+        filename = path+folder+'/'+prefix+'.jsonl'
+        print('loading ANLI...', filename)
+        with open(filename, 'r') as f:
             for line in json_lines.reader(f):
                 guid_id+=1
                 premise = line.get('context')
                 hypothesis = line.get('hypothesis')
                 label = 'entailment' if line.get('label') == 'e' else 'non_entailment'
+                if label == 'entailment':
+                    pos_size+=1
                 examples.append(InputExample(guid=str(guid_id), text_a=premise, text_b=hypothesis, label=label))
     print('size:', len(examples))
-    return examples
+    return examples, pos_size
 
 
 
 
 def load_train_data():
     '''DUC'''
-    duc_examples = get_DUC_examples('train')
+    duc_examples, duc_pos_size = get_DUC_examples('train')
     '''CNN'''
-    cnn_examples = get_CNN_DailyMail_examples('train')
+    cnn_examples, cnn_pos_size = get_CNN_DailyMail_examples('train')
     '''MCTest'''
-    mctest_examples = get_MCTest_examples('train')
+    mctest_examples, mctest_pos_size = get_MCTest_examples('train')
     '''FEVER'''
-    fever_examples = get_FEVER_examples('train')
+    fever_examples, fever_pos_size = get_FEVER_examples('train')
     '''ANLI'''
-    anli_examples = get_ANLI_examples('train')
+    anli_examples, anli_pos_size = get_ANLI_examples('train')
 
     train_examples = duc_examples+cnn_examples+mctest_examples+fever_examples+anli_examples
-    print('train size:', len(train_examples))
+    pos_size = duc_pos_size+cnn_pos_size+mctest_pos_size+fever_pos_size+anli_pos_size
+    print('train size:', len(train_examples), ' pos size:', pos_size)
 
 if __name__ == "__main__":
     load_train_data()
