8c88d10f1fbeba82a84398428e677ad26a604c9d
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-04-21 21:51:19 -0700

    add harsh

--- forfun.py
+++ forfun.py
@@ -4,17 +4,20 @@ from sklearn.metrics import f1_score
 import random
 
 '''random baseline'''
-# out_label_ids = [1]*1847+[0]*909
-# preds = []
-# for i in range(2756):
-#     prob = random.uniform(0, 1)
-#     if prob > 0.5:
-#         preds.append(0)
-#     else:
-#         preds.append(1)
-
 out_label_ids = [1]*1847+[0]*909
-preds = [1]*2756
+preds = []
+for i in range(2756):
+    prob = random.uniform(0, 1)
+    if prob > 0.5:
+        preds.append(0)
+    else:
+        preds.append(1)
+
+# out_label_ids = [1]*1847+[0]*909
+# preds = [1]*2756
+
+f1 = f1_score(out_label_ids, preds, pos_label= 0, average='binary')
+print(f1)
 
 f1 = f1_score(out_label_ids, preds, pos_label= 1, average='binary')
 print(f1)
