166721e5831eea1d3acadfb26c293f8d3df2c204
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-04-25 22:19:25 -0700

    coding

--- train_entail_longformer.py
+++ train_entail_longformer.py
@@ -575,7 +575,7 @@ def main():
     )
     parser.add_argument(
         "--max_seq_length",
-        default=200,
+        default=4096,
         type=int,
         help="The maximum total input sequence length after tokenization. Sequences longer "
         "than this will be truncated, sequences shorter will be padded.",
