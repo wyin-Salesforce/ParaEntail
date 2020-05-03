d118e51b409c07f30fa688e4bd3cb4143e6dfa9a
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-05-02 18:17:52 -0700

    remove the hypo_only bug

--- load_data.py
+++ load_data.py
@@ -355,6 +355,7 @@ def get_MCTest_examples(prefix, hypo_only=False):
     readfile = codecs.open(filename, 'r', 'utf-8')
     guid_id = 0
     pos_size = 0
+    neg_size = 0
     examples = []
     for line in readfile:
         guid_id+=1
@@ -363,35 +364,39 @@ def get_MCTest_examples(prefix, hypo_only=False):
             premise = parts[1]
             hypothesis = parts[2]
             label = 'entailment' if parts[0] == 'entailment' else 'not_entailment'
-            if label == 'entailment':
-                pos_size+=1
+            # if label == 'entailment':
+            #     pos_size+=1
             if len(premise) == 0 or len(hypothesis)==0:
                 # print('MCTest premise:', premise)
                 # print('hypothesis:', hypothesis)
                 continue
 
+            if label == 'entailment':
+                pos_size+=1
+            else:
+                neg_size+=1
             if hypo_only:
                 examples.append(InputExample(guid=prefix+str(guid_id), text_a=hypothesis, text_b=None, label=label))
             else:
                 examples.append(InputExample(guid=prefix+str(guid_id), text_a=premise, text_b=hypothesis, label=label))
     print('MCTest size:', len(examples))
-    if prefix == 'train':
-        new_examples = []
-        new_pos_size = 0
-        new_neg_size = 0
-        for ex in examples:
-            if ex.label == 'not_entailment':
-                if random.uniform(0.0, 1.0) <= pos_size/neg_size:
-                    new_examples.append(ex)
-                    new_neg_size+=1
-            else:
-                new_examples.append(ex)
-                new_pos_size+=1
-        print('>>new pos:neg: ', new_pos_size, new_neg_size)
-        return new_examples, new_pos_size
-    else:
-        return examples, pos_size
-    # return examples, pos_size
+    # if prefix == 'train':
+    #     new_examples = []
+    #     new_pos_size = 0
+    #     new_neg_size = 0
+    #     for ex in examples:
+    #         if ex.label == 'not_entailment':
+    #             if random.uniform(0.0, 1.0) <= pos_size/neg_size:
+    #                 new_examples.append(ex)
+    #                 new_neg_size+=1
+    #         else:
+    #             new_examples.append(ex)
+    #             new_pos_size+=1
+    #     print('>>new pos:neg: ', new_pos_size, new_neg_size)
+    #     return new_examples, new_pos_size
+    # else:
+    #     return examples, pos_size
+    return examples, pos_size
 
 def get_FEVER_examples(prefix, hypo_only=False):
     '''
