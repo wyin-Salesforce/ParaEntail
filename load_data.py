09a497b6fa62e23a41d8009d86b50cdf83d97300
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-05-02 17:52:43 -0700

    new data generation v2

--- load_data.py
+++ load_data.py
@@ -48,10 +48,6 @@ def deal_with_block(block_line_list, filter_label_set, hypo_only=False):
         else:
             examples.append(InputExample(guid='ex', text_a=premise, text_b=neg_hypo, label='not_entailment'))
 
-    print('examples[0].text_b:', examples[0].text_b)
-    print('hypo_only:', hypo_only)
-    exit(0)
-
     return examples, len(pos_hypo_list), len(neg_hypo_list)
 
 def get_summary_examples(path, prefix, hypo_only=False):
