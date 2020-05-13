a10fe7753188fb2e85aa9543f6681a8ae52d458d
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-05-12 17:36:04 -0700

    coding

--- test_longformer_sentenceNLI.py
+++ test_longformer_sentenceNLI.py
@@ -95,8 +95,8 @@ def set_seed(args):
     random.seed(args.seed)
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
-    if args.n_gpu > 0:
-        torch.cuda.manual_seed_all(args.seed)
+    # if args.n_gpu > 0:
+    #     torch.cuda.manual_seed_all(args.seed)
 
 
 def train(args, train_dataset, dev_dataloader, test_dataloader, model, tokenizer):
