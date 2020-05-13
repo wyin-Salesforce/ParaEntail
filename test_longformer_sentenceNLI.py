3b2ec6c07990fb71ce5bd955a2d8303e5a88ee31
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-05-12 17:34:21 -0700

    add SQUAD

--- test_longformer_sentenceNLI.py
+++ test_longformer_sentenceNLI.py
@@ -832,14 +832,14 @@ def main():
         ptvsd.wait_for_attach()
 
     # Setup CUDA, GPU & distributed training
-    if args.local_rank == -1 or args.no_cuda:
-        device = torch.device("cuda" if torch.cuda.is_available() and not args.no_cuda else "cpu")
-        args.n_gpu = 0 if args.no_cuda else torch.cuda.device_count()
-    else:  # Initializes the distributed backend which will take care of sychronizing nodes/GPUs
-        torch.cuda.set_device(args.local_rank)
-        device = torch.device("cuda", args.local_rank)
-        torch.distributed.init_process_group(backend="nccl")
-        args.n_gpu = 1
+    # if args.local_rank == -1 or args.no_cuda:
+    #     device = torch.device("cuda" if torch.cuda.is_available() and not args.no_cuda else "cpu")
+    #     args.n_gpu = 0 if args.no_cuda else torch.cuda.device_count()
+    # else:  # Initializes the distributed backend which will take care of sychronizing nodes/GPUs
+    #     torch.cuda.set_device(args.local_rank)
+    #     device = torch.device("cuda", args.local_rank)
+    #     torch.distributed.init_process_group(backend="nccl")
+    #     args.n_gpu = 1
     args.device = device
 
     # Setup logging
