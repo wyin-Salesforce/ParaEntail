c9d71d1f1ac0ee23948799a60a038c8b2bb633fe
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2021-05-25 16:32:27 -0700

    coding

--- 2021/DocNLI_2_sentNLI/train_MNLI_2_MNLI_commandlines.sh
+++ 2021/DocNLI_2_sentNLI/train_MNLI_2_MNLI_commandlines.sh
@@ -1,5 +1,5 @@
 export BATCHSIZE=32 #32 is also ok
-export EPOCHSIZE=20
+export EPOCHSIZE=5
 export LEARNINGRATE=1e-6
 
 
@@ -13,3 +13,25 @@ CUDA_VISIBLE_DEVICES=0 python -u train_MNLI_2_MNLI.py \
     --learning_rate $LEARNINGRATE \
     --max_seq_length 128 \
     --seed 42 > log.binary.mnli.2.mnli.seed.42.txt 2>&1 &
+
+CUDA_VISIBLE_DEVICES=1 python -u train_MNLI_2_MNLI.py \
+    --task_name rte \
+    --do_train \
+    --do_lower_case \
+    --num_train_epochs $EPOCHSIZE \
+    --train_batch_size $BATCHSIZE \
+    --eval_batch_size 64 \
+    --learning_rate $LEARNINGRATE \
+    --max_seq_length 128 \
+    --seed 16 > log.binary.mnli.2.mnli.seed.16.txt 2>&1 &
+
+CUDA_VISIBLE_DEVICES=2 python -u train_MNLI_2_MNLI.py \
+    --task_name rte \
+    --do_train \
+    --do_lower_case \
+    --num_train_epochs $EPOCHSIZE \
+    --train_batch_size $BATCHSIZE \
+    --eval_batch_size 64 \
+    --learning_rate $LEARNINGRATE \
+    --max_seq_length 128 \
+    --seed 32 > log.binary.mnli.2.mnli.seed.32.txt 2>&1 &
