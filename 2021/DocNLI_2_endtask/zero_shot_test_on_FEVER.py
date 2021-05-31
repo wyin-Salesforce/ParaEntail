e89840f83bc926745448c6636e7410a5ffe9b387
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2021-05-30 20:51:22 -0700

    coding

--- 2021/DocNLI_2_endtask/zero_shot_test_on_FEVER.py
+++ 2021/DocNLI_2_endtask/zero_shot_test_on_FEVER.py
@@ -451,14 +451,29 @@ def main():
         "rte": "classification"
     }
 
+    if args.local_rank == -1 or args.no_cuda:
+        device = torch.device("cuda" if torch.cuda.is_available() and not args.no_cuda else "cpu")
+        n_gpu = torch.cuda.device_count()
+    else:
+        torch.cuda.set_device(args.local_rank)
+        device = torch.device("cuda", args.local_rank)
+        n_gpu = 1
+        # Initializes the distributed backend which will take care of sychronizing nodes/GPUs
+        torch.distributed.init_process_group(backend='nccl')
+    logger.info("device: {} n_gpu: {}, distributed training: {}, 16-bits training: {}".format(
+        device, n_gpu, bool(args.local_rank != -1), args.fp16))
 
-    # args.train_batch_size = args.train_batch_size // args.gradient_accumulation_steps
+    if args.gradient_accumulation_steps < 1:
+        raise ValueError("Invalid gradient_accumulation_steps parameter: {}, should be >= 1".format(
+                            args.gradient_accumulation_steps))
+
+    args.train_batch_size = args.train_batch_size // args.gradient_accumulation_steps
 
     random.seed(args.seed)
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
-    # if n_gpu > 0:
-    #     torch.cuda.manual_seed_all(args.seed)
+    if n_gpu > 0:
+        torch.cuda.manual_seed_all(args.seed)
 
     if not args.do_train and not args.do_eval:
         raise ValueError("At least one of `do_train` or `do_eval` must be True.")
@@ -494,10 +509,10 @@ def main():
     #     len(train_examples) / args.train_batch_size / args.gradient_accumulation_steps) * args.num_train_epochs
     # if args.local_rank != -1:
     #     num_train_optimization_steps = num_train_optimization_steps // torch.distributed.get_world_size()
-
+    device = torch.device('cpu')
     model = RobertaForSequenceClassification(num_labels)
     tokenizer = RobertaTokenizer.from_pretrained(pretrain_model_dir, do_lower_case=args.do_lower_case)
-    model.load_state_dict(torch.load('/export/home/Dataset/BERT_pretrained_mine/paragraph_entail/2021/160k_ANLI_CNNDailyMail_epoch_0.pt'))
+    model.load_state_dict(torch.load('/export/home/Dataset/BERT_pretrained_mine/paragraph_entail/2021/160k_ANLI_CNNDailyMail_epoch_0.pt', map_location=device))
     # model.to(device)
 
     # param_optimizer = list(model.named_parameters())
@@ -570,10 +585,10 @@ def evaluation(dev_dataloader, device, model):
     gold_label_ids = []
     # print('Evaluating...')
     for input_ids, input_mask, segment_ids, label_ids in dev_dataloader:
-        # input_ids = input_ids.to(device)
-        # input_mask = input_mask.to(device)
-        # segment_ids = segment_ids.to(device)
-        # label_ids = label_ids.to(device)
+        input_ids = input_ids.to(device)
+        input_mask = input_mask.to(device)
+        segment_ids = segment_ids.to(device)
+        label_ids = label_ids.to(device)
         gold_label_ids+=list(label_ids.detach().cpu().numpy())
 
         with torch.no_grad():
