6a74c8fc5972c93ce3e795559b041a46ceaddbe4
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2021-06-01 09:55:56 -0700

    coding

---
+++ 2021/DocNLI/baseline_random.py
@@ -0,0 +1,10 @@
+import codecs
+import json
+
+readfile = codecs.open('/export/home/Dataset/para_entail_datasets/test.json', 'r', 'utf-8')
+
+data = json.load(readfile)
+print('len of data:', len(data))
+for dic in data:
+    print(dic)
+    exit(0)
