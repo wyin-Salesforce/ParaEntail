aecb9ff1926e08df49c677bc95fe23bdc5cd06ba
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2021-06-01 16:41:07 -0700

    coding

---
+++ 2021/forfun.py
@@ -0,0 +1,15 @@
+
+from load_data import load_DocNLI
+
+def examples_2_dic(examples):
+    dic = set()
+    for ex in examples:
+        dic.add((ex.text_a, ex.text_b))
+    return dic
+train_examples = load_DocNLI('train', hypo_only=False)
+train_set = examples_2_dic(train_examples)
+dev_examples = load_DocNLI('dev', hypo_only=False)
+dev_set = examples_2_dic(dev_examples)
+
+joint_set = train_set.intersection(dev_set)
+print(len(joint_set))
