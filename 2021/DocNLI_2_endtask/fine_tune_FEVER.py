cfd62cdb4087f3f1d94ffefb093dffc153905fc0
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2021-06-01 19:45:38 -0700

    coding

--- 2021/DocNLI_2_endtask/fine_tune_FEVER.py
+++ 2021/DocNLI_2_endtask/fine_tune_FEVER.py
@@ -686,7 +686,7 @@ if __name__ == "__main__":
 
 '''
 
-CUDA_VISIBLE_DEVICES=0 python -u fine_tune_FEVER.py --task_name rte --do_train --do_lower_case --data_label DUC --num_train_epochs 5 --train_batch_size 4 --eval_batch_size 64 --learning_rate 1e-6 --max_seq_length 512 --seed 42
+CUDA_VISIBLE_DEVICES=0 python -u fine_tune_FEVER.py --task_name rte --do_train --do_lower_case --data_label DUC --num_train_epochs 5 --train_batch_size 32 --eval_batch_size 64 --learning_rate 1e-6 --max_seq_length 128 --seed 42
 
 
 '''
