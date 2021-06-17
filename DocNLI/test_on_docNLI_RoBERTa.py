0ab293714b9aeec7a9d335e573e314b92ff1eee2
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2021-06-17 00:56:04 -0700

    coding

--- DocNLI/test_on_docNLI_RoBERTa.py
+++ DocNLI/test_on_docNLI_RoBERTa.py
@@ -491,8 +491,8 @@ def main():
     output_mode = output_modes[task_name]
 
     test_examples = load_DocNLI('dev', hypo_only=False)
-    random.shuffle(test_examples)
-    test_examples = test_examples[:10000]
+    # random.shuffle(test_examples)
+    # test_examples = test_examples[:10000]
 
     label_list = ["entailment", "not_entailment"]#, "contradiction"]
     num_labels = len(label_list)
@@ -599,7 +599,7 @@ if __name__ == "__main__":
 
 '''
 
-CUDA_VISIBLE_DEVICES=3 python -u test_on_docNLI_RoBERTa.py --task_name rte --do_train --do_lower_case --data_label DUC --num_train_epochs 20 --train_batch_size 32 --eval_batch_size 128 --learning_rate 1e-6 --max_seq_length 512 --seed 42
+CUDA_VISIBLE_DEVICES=4 python -u test_on_docNLI_RoBERTa.py --task_name rte --do_train --do_lower_case --data_label DUC --num_train_epochs 20 --train_batch_size 32 --eval_batch_size 256 --learning_rate 1e-6 --max_seq_length 512 --seed 42
 
 
 '''
